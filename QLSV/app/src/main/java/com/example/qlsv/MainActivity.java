44ffbe564e2f09de86b73b683e625f5457b65cc2
DouTee09 <117714011+DouTee09@users.noreply.github.com>
2024-05-21 11:52:12 +0700

    Updata validate, load data .. when add, update, delete data

--- QLSV/app/src/main/java/com/example/qlsv/MainActivity.java
+++ QLSV/app/src/main/java/com/example/qlsv/MainActivity.java
@@ -67,18 +67,31 @@ public class MainActivity extends AppCompatActivity {
                 String malop = edtmalop.getText().toString();
                 String tenlop = edttenlop.getText().toString();
                 int siso = Integer.parseInt(edtsiso.getText().toString());
-                ContentValues myvalue = new ContentValues();
-                myvalue.put("malop", malop);
-                myvalue.put("tenlop", tenlop);
-                myvalue.put("siso", siso);
-                String msg ="";
-                if(mydatabase.insert("tblop", null, myvalue)  == -1){
-                    msg = "Fail to Insert Record";
+                String sisoStr = edtsiso.getText().toString();
+
+                if(!isSisoUnique(siso)){
+                    Toast.makeText(MainActivity.this, "Sĩ số đã tồn tại", Toast.LENGTH_SHORT).show();
+                } else if (!isTenUnique(tenlop)) {
+                    Toast.makeText(MainActivity.this, "Tên lớp đã tồn tại", Toast.LENGTH_SHORT).show();
+                } else if (!isDataValid(malop, tenlop, sisoStr)) {
+                    Toast.makeText(MainActivity.this, "Vui lòng nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
                 }
                 else{
-                    msg = "Insert record Sucessfully";
+                    ContentValues myvalue = new ContentValues();
+                    myvalue.put("malop", malop);
+                    myvalue.put("tenlop", tenlop);
+                    myvalue.put("siso", siso);
+                    String msg ="";
+                    if(mydatabase.insert("tblop", null, myvalue)  == -1){
+                        msg = "Fail to Insert Record";
+                    }
+                    else{
+                        msg = "Insert record Sucessfully";
+                    }
+                    Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    LoadData();
                 }
-                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+
             }
         });
 
@@ -95,6 +108,7 @@ public class MainActivity extends AppCompatActivity {
                     msg = n + "record is deleted";
                 }
                 Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                LoadData();
             }
         });
 
@@ -103,17 +117,67 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 int siso = Integer.parseInt(edtsiso.getText().toString());
                 String malop = edtmalop.getText().toString();
-                ContentValues myvalue = new ContentValues();
-                int n = mydatabase.update("tblop", myvalue, "malop = ?", new String[]{malop});
-                String msg = "";
-                if(n == 0){
-                    msg = "No record to Update";
+
+                if(!isSisoUnique(siso)){
+                    Toast.makeText(MainActivity.this, "Sĩ số đã tồn tại", Toast.LENGTH_SHORT).show();
                 }
                 else{
-                    msg = n + "record is updated";
+                    ContentValues myvalue = new ContentValues();
+                    myvalue.put("siso", siso);
+                    int n = mydatabase.update("tblop", myvalue, "malop = ?", new String[]{malop});
+                    String msg = "";
+                    if(n == 0){
+                        msg = "No record to Update";
+                    }
+                    else{
+                        msg = n + "record is updated";
+                    }
+                    Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    LoadData();
+
                 }
-                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
             }
         });
+        LoadData();
+    }
+
+    private void LoadData(){
+        edtmalop.setText("");
+        edttenlop.setText("");
+        edtsiso.setText("");
+        mylist.clear();
+        Cursor c = mydatabase.query("tblop", null, null, null, null, null, null);
+        c.moveToNext();
+        String data = "";
+        while(c.isAfterLast() == false){
+            data = c.getString(0)+" - "+c.getString(1)+" - "+c.getString(2);
+            c.moveToNext();
+            mylist.add(data);
+        }
+        c.close();
+        myadapter.notifyDataSetChanged();
+    }
+
+    private boolean isSisoUnique(int siso) {
+        Cursor cursor = mydatabase.rawQuery("SELECT COUNT(*) FROM tblop WHERE siso = ?", new String[]{String.valueOf(siso)});
+        cursor.moveToFirst();
+        int count = cursor.getInt(0);
+        cursor.close();
+        return count == 0;
+    }
+
+    private boolean isTenUnique(String tenlop) {
+        Cursor cursor = mydatabase.rawQuery("SELECT COUNT(*) FROM tblop WHERE tenlop = ?", new String[]{String.valueOf(tenlop)});
+        cursor.moveToFirst();
+        int count = cursor.getInt(0);
+        cursor.close();
+        return count == 0;
+    }
+
+    private boolean isDataValid(String malop, String tenlop, String siso) {
+        if (malop.isEmpty() || tenlop.isEmpty() || siso.isEmpty()) {
+            return false;
+        }
+        return true;
     }
 }

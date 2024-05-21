5f2ce28f141651fdc60847a9b4d98df66ce067b5
DouTee09 <117714011+DouTee09@users.noreply.github.com>
2024-05-21 11:05:43 +0700

    Updata Connect database, event Insert, Delete, Update

--- QLSV/app/src/main/java/com/example/qlsv/MainActivity.java
+++ QLSV/app/src/main/java/com/example/qlsv/MainActivity.java
@@ -1,6 +1,16 @@
 package com.example.qlsv;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +18,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    EditText edtmalop, edttenlop, edtsiso;
+    Button btInsert, btDelete, btUpdate;
+    ListView lv;
+    ArrayList<String> mylist;
+    ArrayAdapter<String> myadapter;
+    SQLiteDatabase mydatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +37,83 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        edtmalop = findViewById(R.id.edtClassID);
+        edttenlop = findViewById(R.id.edtClassName);
+        edtsiso = findViewById(R.id.edtNumberStudent);
+
+        btInsert = findViewById(R.id.btInsert);
+        btDelete = findViewById(R.id.btDelete);
+        btUpdate = findViewById(R.id.btUpdate);
+
+        lv = findViewById(R.id.lvClass);
+        mylist = new ArrayList<>();
+        myadapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mylist);
+        lv.setAdapter(myadapter);
+
+        mydatabase = openOrCreateDatabase("qlsinhvien.db", MODE_PRIVATE, null);
+        try{
+            String sql = "create table tblop(malop TEXT primary key, tenlop TEXT, siso INTEGER)";
+            mydatabase.execSQL(sql);
+        }
+        catch (Exception e){
+            Log.e("Error", "Table đã tồn tại");
+        }
+
+        btInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String malop = edtmalop.getText().toString();
+                String tenlop = edttenlop.getText().toString();
+                int siso = Integer.parseInt(edtsiso.getText().toString());
+                ContentValues myvalue = new ContentValues();
+                myvalue.put("malop", malop);
+                myvalue.put("tenlop", tenlop);
+                myvalue.put("siso", siso);
+                String msg ="";
+                if(mydatabase.insert("tblop", null, myvalue)  == -1){
+                    msg = "Fail to Insert Record";
+                }
+                else{
+                    msg = "Insert record Sucessfully";
+                }
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String malop = edtmalop.getText().toString();
+                int n = mydatabase.delete("tblop", "malop = ?", new String[]{malop});
+                String msg = "";
+                if(n == 0){
+                    msg = "No record to Delete";
+                }
+                else{
+                    msg = n + "record is deleted";
+                }
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int siso = Integer.parseInt(edtsiso.getText().toString());
+                String malop = edtmalop.getText().toString();
+                ContentValues myvalue = new ContentValues();
+                int n = mydatabase.update("tblop", myvalue, "malop = ?", new String[]{malop});
+                String msg = "";
+                if(n == 0){
+                    msg = "No record to Update";
+                }
+                else{
+                    msg = n + "record is updated";
+                }
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
